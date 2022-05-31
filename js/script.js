e06b40c3c0a4b35a78dc4036f273230ddb68ceca
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-31 11:21:20 +0200

    aggiornamento

--- js/script.js
+++ js/script.js
@@ -276,8 +276,8 @@ const myItems = new Vue ({
        searchContact(){
             if (this.writeContact !== " "){
                 for (let i = 0; i < this.contacts.length; i++){
-                    let stringa = this.contacts[i].name;
-                    if (stringa.includes(this.writeContact)){
+                    let stringa = this.contacts[i].name.toLowerCase();
+                    if (!stringa.includes(this.writeContact)){
                         this.contacts[i].visible = false;
                     }
                 }
